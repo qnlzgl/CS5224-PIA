e79a801d08cc523c522e1b04215b7b73ee466fea
Zhao Guanlun <guanlun_zhao@mymail.sutd.edu.sg>
2019-04-19 18:44:14 +0800

    Updates

---
+++ precache-manifest.31370af9b7a5d4b9b4764d3cb5218031.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1cdb3c1081ed16119272130b5c02bb4",
+    "url": "/CS5224-PIA/static/media/icon-back.e1cdb3c1.svg"
+  },
+  {
+    "revision": "c8aa5124ee14883dc5cb",
+    "url": "/CS5224-PIA/static/js/runtime~main.c8aa5124.js"
+  },
+  {
+    "revision": "e00b7927ed039f42a0b6",
+    "url": "/CS5224-PIA/static/js/main.e00b7927.chunk.js"
+  },
+  {
+    "revision": "d1001949a5c440b00f00",
+    "url": "/CS5224-PIA/static/js/2.d1001949.chunk.js"
+  },
+  {
+    "revision": "e00b7927ed039f42a0b6",
+    "url": "/CS5224-PIA/static/css/main.75b84237.chunk.css"
+  },
+  {
+    "revision": "d1001949a5c440b00f00",
+    "url": "/CS5224-PIA/static/css/2.c8b62d4b.chunk.css"
+  },
+  {
+    "revision": "0504b59e4f7e7ff49a295ee08de1f28d",
+    "url": "/CS5224-PIA/index.html"
+  }
+];
